490a91542f18e1329c18408bbaf09f80d84b5969
noah <nuoya1996@gmail.com>
2025-11-11 15:10:12 -0500

    first commit

---
+++ src/main/java/com/hamiltonjewelers/ns_sf_connector/NsSfConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.hamiltonjewelers.ns_sf_connector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsSfConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NsSfConnectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hamiltonjewelers/ns_sf_connector/NsSfConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamiltonjewelers.ns_sf_connector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NsSfConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
